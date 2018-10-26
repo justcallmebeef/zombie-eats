c0db2b7ecb796ceabc444d85c0f804bc2783d1cc
Meredith Fontana <meredith@Merediths-MacBook-Pro.local>
2018-10-26 12:06:57 -0600

    added cards under header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header";
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <nav className="navbar navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">Shopping Cart</a>
+      <nav className="navbar navbar-light ">
+        <a className="navbar-brand" href="#">Zombie Eats</a>
       </nav>
       <Header />
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Description from './Description';
 
 const Header = () => {
     return (
-        
+        <div>
        <div className="Header">
+       
        </div> 
+       <div>
+       <Description />
+       </div>
+       </div>
     )
 }
 
